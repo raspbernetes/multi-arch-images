9b1ffd94ee356dfa4c5c8f94b0a44051422e792b
Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
2021-08-26 11:57:17 +1000

    feat: improve workflow (#221)
    * feat: improve workflow
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    * fix: revert commit
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    * fix: remove EOL at EOF lint
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    * fix: path was incorrect
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>
    
    * fix: deprecate prometheus-operator-config-reloader
    
    Signed-off-by: Michael Fornaro <20387402+xUnholy@users.noreply.github.com>

---
+++ build/botkube/latest-version.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+version=$(curl -sL "https://api.github.com/repos/infracloudio/botkube/tags" | jq -r ".[0] .name")
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
